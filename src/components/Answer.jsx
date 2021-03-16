29bfa5f9d097a69bb25f5f5f8f1dfe7266741106
ykanno0521 <54009743+ykanno0521@users.noreply.github.com>
2021-03-17 07:35:19 +0900

    Material-UIを設定

--- src/components/Answer.jsx
+++ src/components/Answer.jsx
@@ -1,21 +1,30 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
+import { makeStyles, createStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
 
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    '& > *': {
-      margin: theme.spacing(1),
-    },
-  },
-}));
+const useStyles = makeStyles( () => (
+  createStyles({
+    "button": {
+      borderColor: "#FFB549",
+      color: '#FFB549',
+      fontWeight: 600,
+      marginBottom: "8px",
+      "&:hover": {
+        backGroundColor: "#FF549",
+        color: "#fff",
+      }
+    }
+  })
+));
 
 const Answer = (props) => {
-  // const classes = useStyles();
+  const classes = useStyles();
   
   return (
-    <Button variant="contained" color="primary" onClick= { () => props.select(props.content, props.nextId)}>
+    <Button
+      className={ classes.button }
+      variant="outlined" onClick={() => props.select(props.content, props.nextId)}>
       {props.content }
     </Button>
   )
