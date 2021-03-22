d65c8e00179c008209312c2908801e6dee1e0de2
ykanno0521 <54009743+ykanno0521@users.noreply.github.com>
2021-03-23 06:51:22 +0900

    REST API作成、async付きの即時関数を使う

---
+++ src/firebase/config.js
@@ -0,0 +1,12 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyA8-U3a9pasPhD9yY8v3EjpznKnmP1qGag",
+  authDomain: "chatbot-demo-554f8.firebaseapp.com",
+  projectId: "chatbot-demo-554f8",
+  storageBucket: "chatbot-demo-554f8.appspot.com",
+  messagingSenderId: "206342433964",
+  appId: "1:206342433964:web:95335dcb4b2cde01b6c309",
+  measurementId: "G-D5Q38ZFJ1L"
+};
+
+export default firebaseConfig
